d4497a38c1ed28c23e09e354899909735212b7f5
Ilya Balashov <ibalashov24@gmail.com>
2019-11-23 00:58:23 +0300

    Add (broken) Sobel filter

--- Lab2/LabAssignment.asm
+++ Lab2/LabAssignment.asm
@@ -38,38 +38,81 @@ Kernel PROC ; [RCX] - pDst
     ; Gx = 2*(z4 - z0) + z1 + z7 - z3 - z9
     ; Gy = 2*(z2 - z8) + z2 + z3 - z7 - z9
     ; Тогда итоговое значение после обработки будет равно [RCX] = sqrt(Gx^2 + Gy^2)
+
+    vzeroall
+
+    ; Сохранять ymm6 и ymm7, ymm8
     
-    ; Читаем по 8 экземпляров:
-    vpmovsxbd ymm0, qword ptr [rdx]             ; z1
+    ; Читаем по 8 экземпляров каждого элемента матрицы:
     vpmovsxbd ymm1, qword ptr [rdx + 2]         ; z3
     vpmovsxbd ymm2, qword ptr [rdx + r8]        ; z4
     vpmovsxbd ymm3, qword ptr [rdx + r8 + 2]    ; z6
     vpmovsxbd ymm4, qword ptr [rdx + 2*r8]      ; z7
     vpmovsxbd ymm5, qword ptr [rdx + 2*r8 + 2]  ; z9
+    vpmovsxbd ymm6, qword ptr [rdx + 1]         ; z2
+    vpmovsxbd ymm7, qword ptr [rdx + 2*r8 + 1]  ; z8
+    vpmovsxbd ymm8, qword ptr [rdx]             ; z1
 
-    vcvtdq2ps ymm0, ymm0
+    ; Конвертируем целые числа в вещественные
     vcvtdq2ps ymm1, ymm1
     vcvtdq2ps ymm2, ymm2
     vcvtdq2ps ymm3, ymm3
     vcvtdq2ps ymm4, ymm4
     vcvtdq2ps ymm5, ymm5
+    vcvtdq2ps ymm6, ymm6
+    vcvtdq2ps ymm7, ymm7
+    vcvtdq2ps ymm8, ymm8
 
     ; Умножаем элементы средней строки на 2
     vmulps ymm2, ymm2, MultiplierTwo 
     vmulps ymm3, ymm3, MultiplierTwo
+    vmulps ymm6, ymm6, MultiplierTwo
+    vmulps ymm7, ymm7, MultiplierTwo
 
-    ; Применяем отрицание к элементам, которые в матрице Собеля со знаком "минус"
+    ; Применяем отрицание к элементам, которые в матрице Gx со знаком "минус"
     vmulps ymm1, ymm1, MultiplierMinusOne
     vmulps ymm3, ymm3, MultiplierMinusOne
     vmulps ymm5, ymm5, MultiplierMinusOne
 
-    ; Производим сложение
+    ; Производим сложение и получаем Gx
     vaddps ymm0, ymm0, ymm1
     vaddps ymm0, ymm0, ymm2
     vaddps ymm0, ymm0, ymm3
     vaddps ymm0, ymm0, ymm4
     vaddps ymm0, ymm0, ymm5
+    vaddps ymm0, ymm0, ymm8
 
+    ; Gx^2
+    vmulps ymm0, ymm0, ymm0
+
+        
+    ; Получаем Gy
+
+    vmulps ymm1, ymm1, MultiplierMinusOne
+    vmulps ymm4, ymm4, MultiplierMinusOne
+    vmulps ymm5, ymm5, MultiplierMinusOne
+    vmulps ymm7, ymm7, MultiplierMinusOne
+
+    vaddps ymm9, ymm9, ymm8
+    vaddps ymm9, ymm9, ymm6
+    vaddps ymm9, ymm9, ymm1
+    vaddps ymm9, ymm9, ymm4
+    vaddps ymm9, ymm9, ymm5
+    vaddps ymm9, ymm9, ymm7
+
+    ; Gy^2
+    vmulps ymm9, ymm9, ymm9
+
+    ; Gx^2 + Gy^2
+    vaddps ymm0, ymm0, ymm9
+
+    ; sqrt(Gx^2 + Gy^2)
+    vsqrtps ymm0, ymm0
+
+    
+
+    ; TODO: Optimize it!!!
+     
     ; Конветируем вешественные числа в целые DWORD
     vcvtps2dq ymm0, ymm0
     ; Упаковываем результат из DWORD в BYTE
